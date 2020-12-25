f195894970d1e19c127fdf9e99a3868f94df2fa4
Yasser <yasser@leanium.io>
2020-12-25 23:15:14 +0300

    Initial Commit

---
+++ Weather-SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather-SwiftUI
+//
+//  Created by Apple on 25/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
